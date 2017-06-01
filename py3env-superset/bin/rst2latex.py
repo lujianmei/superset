37de7e7bdb78f0f34657c94479764d14197e8fe4
lujianmei <anysky130@163.com>
2017-06-01 16:36:28 +0800

    changed

---
+++ py3env-superset/bin/rst2latex.py
@@ -0,0 +1,4 @@
+#!/Users/kevin/workspace/github/superset/py3env-superset/bin/python
+# EASY-INSTALL-SCRIPT: 'docutils==0.13.1','rst2latex.py'
+__requires__ = 'docutils==0.13.1'
+__import__('pkg_resources').run_script('docutils==0.13.1', 'rst2latex.py')
--- superset/config.py
+++ superset/config.py
@@ -130,14 +130,14 @@ PUBLIC_ROLE_LIKE_GAMMA = False
 # Babel config for translations
 # ---------------------------------------------------
 # Setup default language
-BABEL_DEFAULT_LOCALE = 'en'
+BABEL_DEFAULT_LOCALE = 'zh'
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = 'babel/translations'
 # The allowed translation for you app
 LANGUAGES = {
     'en': {'flag': 'us', 'name': 'English'},
     # 'fr': {'flag': 'fr', 'name': 'French'},
-    # 'zh': {'flag': 'cn', 'name': 'Chinese'},
+    'zh': {'flag': 'cn', 'name': 'Chinese'},
 }
 # ---------------------------------------------------
 # Image and file configuration
